2091be8283b43dd1a640e4d6ad8bd727e48e6687
Tai Kedzierski <dch.tai gmail>
2018-04-26 20:26:12 +0100

    Improve install script
    * remove redundant check on `cd`
    * add positive response options
    * only install normal or xkcd variants

--- install.sh
+++ install.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
+set -euo pipefail
+
 [[ $UID -gt 0 ]] && {
 	echo "You are not root"
 	exit 1
 }
 
-cd "$(dirname "$0")" || {
-	echo "Please switch to the alpacka directory"
-	exit 1
-}
+cd "$(dirname "$0")"
 
-if [[ "$@" =~ xkcd ]]; then
+if [[ "$*" =~ xkcd ]]; then
 	bash bin/pafx --help
-	read -p "Are you sure?  y/N> "
-	[[ $REPLY = 'y' ]] && {
+	read -p "Are you sure you want to install the XKCD installer? y/N> "
+	[[ $REPLY =~ ^(y|Y|yes|YES)$ ]] && {
 		cp bin/pafx /usr/local/bin/pafx
 		echo "The 'pafx' command is now available. You masochist."
 	}
 	exit
+else
+	cp bin/pafs bin/paf /usr/local/bin/
+
+	echo "The 'paf' and 'pafs' commands are now available."
 fi
-
-cp bin/pafs bin/paf /usr/local/bin/
-
-echo "The 'paf' and 'pafs' commands are now available."
